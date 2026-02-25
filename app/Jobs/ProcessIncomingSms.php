b61c170f2d996dafd8cc8d4644c64d6e51123aa5
Michael Kilunga <mickworks30@gmail.com>
2026-02-25 20:40:36 +0300

    chore: Add Log facade import to ProcessIncomingSms job.

--- app/Jobs/ProcessIncomingSms.php
+++ app/Jobs/ProcessIncomingSms.php
@@ -4,6 +4,7 @@ namespace App\Jobs;
 
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Log;
 
 use App\Models\User;
 use App\Models\Message;
